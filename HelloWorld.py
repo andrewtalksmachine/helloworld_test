1348baf0e812b0e8b724ad1ba7abe12451c81b0f
andrewtalksmachine <andrewtalksmachine@gmail.com>
2018-07-17 21:31:24 -0700

    Made changes to store name,age and calculate year born

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,16 +1,18 @@
-ls#test this IDE
-print "Hello World!"
-print "Hello World, Again!"
-
+#test this IDE
 
 from datetime import datetime 
-
 
 today = datetime.today()
 time_now = datetime.now()
 
-print "today is " + str(today.month) + ' ' + str (today.day) + ' ' + str(today.year) 
-print "hello world - 3"
-print "hello world - 4"
-print "hello world - 5"
-print "adding another update here"
+
+print "Hello there and welcome to this new app!"
+name = raw_input("What is your name? ").lower()
+age = int(raw_input("What is your age? "))
+year_born = today.year - age 
+
+#print "Today is " + str(today.month) + ' ' + str (today.day) + ' ' + str(today.year) + ' ' + name
+print "Hello %s, today is %02d %02d %04d" % (name,today.month, today.day, today.year)
+print "Since you are %d years old you were probably born in the year %d" % (age, year_born)
+
+
