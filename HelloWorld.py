fe5b48de76c442eb0a30d819c1b84ef1e3b0ccd3
andrewtalksmachine <andrewtalksmachine@gmail.com>
2018-07-17 17:40:07 -0700

    commit number 1. pls work.

---
+++ HelloWorld.py
@@ -0,0 +1,16 @@
+#test this IDE
+print "Hello World!"
+print "Hello World, Again!"
+
+
+from datetime import datetime 
+
+
+today = datetime.today()
+time_now = datetime.now()
+
+print "today is " + str(today.month) + ' ' + str (today.day) + ' ' + str(today.year) 
+print "hello world - 3"
+print "hello world - 4"
+print "hello world - 4"
+
